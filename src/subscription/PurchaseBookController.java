00d8107c8a71de75b04dd6d89a185de9f0f5880f
aidan <ajf6238@psu.edu>
2023-10-08 15:59:50 -0400

    fleshed out method stubs (slightly

--- src/subscription/PurchaseBookController.java
+++ src/subscription/PurchaseBookController.java
@@ -1,10 +1,18 @@
 package subscription;
 
+
 public class PurchaseBookController {
     /**
      * This is the default constructor for the PurchaseBookController class
      */
+    private PurchaseBook book;
+    private PurchaseBookInterface pbi;
+    private UserFinanceData userFinanceData;
+
     public PurchaseBookController(){
+        this.pbi = new PurchaseBookInterface();
+        book = pbi.displayBook();
+
 
     }
 }
