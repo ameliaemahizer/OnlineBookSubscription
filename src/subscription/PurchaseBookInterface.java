ddaa78ea6d8686e889434a363512760410097980
aidan <ajf6238@psu.edu>
2023-10-08 15:59:50 -0400

    fleshed out method stubs (slightly

--- src/subscription/PurchaseBookInterface.java
+++ src/subscription/PurchaseBookInterface.java
@@ -1,9 +1,12 @@
 package subscription;
-
-public interface PurchaseBookInterface {
-    /**
-     * displays purchase info
-     */
-    void displayPurchaseInfo();
-
+import java.util.Scanner;
+public class PurchaseBookInterface {
+    public PurchaseBook book1;
+    public PurchaseBook displayBook(){
+        Scanner s = new Scanner (System.in);
+        System.out.println("enter book name: ");
+        String bookName = s.next();
+        book1.setBookTitle(bookName);
+        return book1;
+    }
 }
