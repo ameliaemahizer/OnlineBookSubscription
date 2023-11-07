f88bea11fd068829587067d1fc726110021fcf3e
ake5280 <ake5280@psu.edu>
2023-11-07 14:06:25 -0500

    finished adding basic sql queries, added books and login to database

--- src/login/LoginController.java
+++ src/login/LoginController.java
@@ -22,15 +22,24 @@ public class LoginController {
     }
 
     public static void main(String[] args) {
-        try{
+        try {
             Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-            Connection conn= DriverManager.getConnection("jdbc:ucanaccess:C:\\Users\\ameli\\Desktop\\stupid\\OnlineBookSubscription\\src\\database\\BookClubDatabase1.accdb");//needs changed depending on person
+            Connection conn = DriverManager.getConnection("jdbc:ucanaccess://src/database/BookClubDatabase1.accdb");//needs changed depending on person
             Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("select Book.ID, Book.Title from Book");
+            ResultSet rs = st.executeQuery(("select Book.ID, Book.Title, " +
+                    "Genre.Genre from Book, Genre " +
+                    " where Book.Genre=Genre.ID" ));
+            while(rs.next()){
+                System.out.println("Book ID: " + rs.getString(1)
+                        + "; Name: " + rs.getString(2) + "; Genre: "
+                        + rs.getString(3));
+            }
+            conn.commit();
+            conn.close();
 
-        }
-        catch(Exception ee){System.out.println(ee);}
-        }
-        }
 
+        } catch (Exception ee) {
+            System.out.println(ee);
+        }
+    }
 }
