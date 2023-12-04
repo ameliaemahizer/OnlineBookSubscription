c438eab520c655fa628c002c8170dd98606fe90c
jackfitz88 <99668827+jackfitz88@users.noreply.github.com>
2023-12-03 20:07:08 -0500

    Made discussion board interface

--- src/discussion/DiscussionBoardController.java
+++ src/discussion/DiscussionBoardController.java
@@ -1,55 +1,18 @@
 package discussion;
+import javax.swing.SwingUtilities;
+
 import login.UserModel;
 
 
 public class DiscussionBoardController {
-
-    private UserModel userModelName;
-    private String postTitle;
-    private String mainPost;
-    private final DiscussionReplyModel reply = new DiscussionReplyModel();
-    private final DiscussionPostModel discussionPost = new DiscussionPostModel();
     private DiscussionBoardInterface discussionBoardInterface;
 
-    /**
-     * This is the default constructor for the DiscussionBoard class
-     */
     public DiscussionBoardController() {
-        createDiscussionPost();
-    }
-
-    /**
-     * Creates main discussion post
-     * @return right now, returns null, but it will return the combined main post
-     */
-    public DiscussionPostModel createDiscussionPost(){
         this.discussionBoardInterface = new DiscussionBoardInterface();
-
-        System.out.println("What is the title of the post?");
-        discussionPost.setPostTitle();
-
-        System.out.println("What is the body of the post?");
-        discussionPost.setMainPost();
-
-        System.out.println("Post successfully posted!");
-        this.discussionBoardInterface.displayDiscussionPost(discussionPost);
-
-        createDiscussionReply();
-
-        return discussionPost;
-        
+        this.discussionBoardInterface.getDiscussionBoard();
     }
 
-    /**
-     * Creates reply
-     * @return right now, return null, but it will return the combined reply
-     */
-    public DiscussionReplyModel createDiscussionReply(){
-
-        reply.setPostReply("That's a great insight! I agree completely.");
-
-        System.out.println("Reply successfully posted!");
-        this.discussionBoardInterface.displayDiscussionReply(reply);
-        return reply;
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new DiscussionBoardController());
     }
 }
